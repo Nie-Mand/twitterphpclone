59331ad3f6a1f57570d535cc1e32b659ba533a74
Nie-Mand <chill.out.x19@gmail.com>
2020-06-19 14:05:01 +0100

    Update add_tweet.php

--- add_tweet.php
+++ add_tweet.php
@@ -1,19 +1,19 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "dagotthatdope", "twitter");
+mysql_connect("localhost", "root", "");
+mysql_select_db("twitter");
 $username = $_POST["username"];
 $password = $_POST["password"];
 $content = $_POST["content"];
 $sql = "select id from users where username = '$username'";
-$res = mysqli_query($conn, $sql);
-if (mysqli_num_rows($res) == 0) die ("User does not Exist");
+$res = mysql_query($sql);
+if (mysql_num_rows($res) == 0) die ("User does not Exist");
 $sql = "select id from users where username = '$username' and password = '$password'";
-$res = mysqli_query($conn, $sql);
-if (mysqli_num_rows($res) == 0) die ("Password is Incorrcet");
-$id = mysqli_fetch_array($res)["id"];
+$res = mysql_query($sql);
+if (mysql_num_rows($res) == 0) die ("Password is Incorrcet");
+$id = mysql_fetch_array($res)["id"];
 $date = date("Y-m-d");
 $sql = "insert into tweets (iduser, content, datet) values('$id', '$content', '$date')";
-$res = mysqli_query($conn, $sql);
+$res = mysql_query($sql);
 echo "Tweeted Succefully";
-echo mysqli_error($conn);
-mysqli_close($conn);
+mysql_close();
 ?>
