dc91045cd18a2b4815cebcf9e5bca4f2ee76adcb
Nie-Mand <chill.out.x19@gmail.com>
2020-06-19 14:07:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 echo '<h1>Feeds</h1>';
-$conn = mysqli_connect("localhost", "root", "dagotthatdope", "twitter");
+mysql_connect("localhost", "root", "");
+mysql_select_db("twitter");
 $sql = "select name, content from users u, tweets t where u.id = t.iduser order by datet";
-$res = mysqli_query($conn, $sql);
+$res = mysql_query($sql);
 echo "<table><tr><th>Name</th><th>Tweets</th></tr>";
-while ($r = mysqli_fetch_array($res)) {
+while ($r = mysql_fetch_array($res)) {
   $name = $r["name"];
   $content = $r["content"];
   echo "<tr><td>$name</td><td>$content</td></tr>";
 }
 echo '</table>';
-mysqli_close($conn);
+mysql_close();
 ?>
