7a50622c61edeec61c62dc74da690adb060bef6f
Nie-Mand <chill.out.x19@gmail.com>
2020-06-21 18:27:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo '<h1>Feeds</h1>';
 mysql_connect("localhost", "root", "");
 mysql_select_db("twitter");
-$sql = "select name, content from users u, tweets t where u.id = t.iduser order by datet";
+$sql = "select name, content from users u, tweets t where u.iduser = t.iduser order by datet";
 $res = mysql_query($sql);
 echo "<table><tr><th>Name</th><th>Tweets</th></tr>";
 while ($r = mysql_fetch_array($res)) {
